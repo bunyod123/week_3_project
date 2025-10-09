2a23277835a08e6587a5c119c8fb20364de81c8a
bunyod123 <bunyodbek9690@gmail.com>
2025-10-09 20:06:27 +0900

    s

---
+++ script/new.py
@@ -0,0 +1,6 @@
+import os,sys
+
+sys.path.append(r"C:\Users\bunyo\OneDrive\Desktop\3_week_project\source")
+
+
+# from loggingg import logger
--- source/loggingg.py
+++ source/loggingg.py
@@ -1,12 +1,17 @@
+# src/logger.py
 import logging
 
-def logs(self):
+# LOG_DIR = "logs"
+# os.makedirs(LOG_DIR, exist_ok=True)
+# LOG_FILE = os.path.join(LOG_DIR, "ml_pipeline.log")
 
-    logging.basicConfig(
+logging.basicConfig(
     filename=r"C:\Users\bunyo\OneDrive\Desktop\3_week_project\log\otherInfo.log",
     filemode='a',
     level=logging.INFO,
-    format="%(asctime)s-%(levelname)s-%(message)s"
-
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
+
+logger = logging.getLogger("otherInfo.log")
+
 
